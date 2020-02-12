a9b41e4e95a74759c0a79632db02a96f08108ab9
Ikuzen <yannis.samba@gmail.com>
2020-02-12 10:14:04 +0100

    creating graph class

---
+++ Graph.js
@@ -0,0 +1,10 @@
+"use strict";
+class Graph {
+    // defining vertex array and 
+    // adjacent list 
+    constructor(noOfVertices) {
+        this.noOfVertices = noOfVertices;
+        this.AdjList = new Map();
+    }
+}
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiR3JhcGguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJHcmFwaC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO0FBQUEsTUFBTSxLQUFLO0lBQ1AsNkJBQTZCO0lBQzdCLGlCQUFpQjtJQUNqQixZQUFZLFlBQVk7UUFDcEIsSUFBSSxDQUFDLFlBQVksR0FBRyxZQUFZLENBQUM7UUFDakMsSUFBSSxDQUFDLE9BQU8sR0FBRyxJQUFJLEdBQUcsRUFBRSxDQUFDO0lBQzdCLENBQUM7Q0FVSiJ9
